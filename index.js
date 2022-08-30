03156114e274564c1ea034b89ea8a186e6079215
hanxl <hanxl.james@gmail.com>
2022-08-30 09:03:42 -0500

    commit tree b511633afdef99af068f0c5c2b7b7670a3b54d74

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name)
+};
+
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name)
+};
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop()
+};
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+};
+
+const appendCat = (name) => {
+    const copyofCats = cats.slice();
+    copyofCats.push(name)
+    return copyofCats
+};
+
+const prependCat = (name) => {
+    const copyofCats = cats.slice();
+    copyofCats.unshift(name)
+    return copyofCats
+};
+
+const removeLastCat = () => {
+    const copyofCats = cats.slice();
+    copyofCats.pop()
+    return copyofCats
+};
+
+const removeFirstCat = () => {
+    const copyofCats = cats.slice();
+    copyofCats.shift()
+    return copyofCats
+};
